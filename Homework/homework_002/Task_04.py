eb3e29ea5f28dea0c4fa2116f82d12912de5a785
iam23db <iam23db@yandex.ru>
2022-09-29 22:00:07 +0300

    add format for code

--- Homework/homework_002/Task_04.py
+++ Homework/homework_002/Task_04.py
@@ -1,8 +1,10 @@
 # 4. Задайте список из 2N+1 элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры.
 number = int(input('Введите N: '))
-elements = [i for i in range (-number, number+1)]
-positions = list(map(int, input('введите позиции для поиска произведения через пробел: ').split()))
+elements = [i for i in range(-number, number+1)]
+positions = list(map(int, input(
+    'введите позиции для поиска произведения через пробел: ').split()))
 composition = 1
 for i in positions:
-    composition*=elements[i-1]
-print(f'Произведение элементов списка {elements}, находящихся на позициях {positions} = {composition}')
+    composition *= elements[i-1]
+print(
+    f'Произведение элементов списка {elements}, находящихся на позициях {positions} = {composition}')
--- Homework/homework_002/Task_05.py
+++ Homework/homework_002/Task_05.py
@@ -1,7 +1,7 @@
 # Реализуйте алгоритм перемешивания списка.
 import random
 from random import randint
-first_list = [randint(0,9999) for i in range(randint(2,12))]
+first_list = [randint(0, 9999) for i in range(randint(2, 12))]
 print(first_list)
 random.shuffle(first_list)
 print(first_list)
