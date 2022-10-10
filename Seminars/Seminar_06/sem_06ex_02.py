4ab9cb8b14689c7c3c7e9fa3f967edd92b0232ee
iam23db <iam23db@yandex.ru>
2022-10-10 20:49:11 +0300

    add input for 2-nd task

--- Seminars/Seminar_06/sem_06ex_02.py
+++ Seminars/Seminar_06/sem_06ex_02.py
@@ -2,6 +2,6 @@
 # последовательности.
 # Пример:
 # [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10]
-user_list = [1, 2, 3, 5, 1, 5, 3, 10]
+user_list = list(map(int, input().split()))
 answer = [i for i in user_list if user_list.count(i)==1]
 print (answer)
