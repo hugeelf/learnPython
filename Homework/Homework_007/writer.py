4c9fab48a500eb902cd6c537f7c8332620a0a15e
iam23db <iam23db@yandex.ru>
2022-10-14 16:16:21 +0300

    fixed write to file

--- Homework/Homework_007/writer.py
+++ Homework/Homework_007/writer.py
@@ -1,3 +1,5 @@
 def add_contact(user_data):
     with open('phone_book.txt', 'a', encoding='utf-8') as pb:
+        temp = user_data.split('+')
+        user_data = temp[0]+'|| +'+temp[1]
         pb.write('\n' + user_data.title())
